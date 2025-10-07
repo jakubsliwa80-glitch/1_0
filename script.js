e6ff3cb852dd0297c64869e76b463d5b6b6fc514
jakubsliwa80-glitch <jakubsliwa80@gmail.com>
2025-10-07 08:04:00 +0000

    Com1_1.2

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const button = document.getElementById("ex1_button");
 const contentContainer = document.getElementById("ex1_content");
+const textField = document.getElementById("ex2_text");
+const validationOutput = document.getElementById("ex2_content");
 
 function generateAndDisplayDigits() {
   let digits = [];
@@ -14,3 +16,41 @@ function generateAndDisplayDigits() {
 }
 
 button.addEventListener("click", generateAndDisplayDigits);
+
+function validatePhoneNumber() {
+  
+  const text = textField.value.trim();
+  const len = text.length;
+
+  
+  validationOutput.textContent = "";
+
+  
+  if (len !== 9) {
+    validationOutput.textContent = "Długość numeru musi być równa 9";
+    validationOutput.style.color = "orange";
+    return; 
+  }
+  
+  const containsLetters = /[a-zA-Z]/.test(text);
+  if (containsLetters) {
+    validationOutput.textContent = "Numer nie może zawierać liter";
+    validationOutput.style.color = "red";
+    return;
+  }
+
+  
+  const containsSpecialChars = /[^0-9]/.test(text);
+  if (containsSpecialChars) {
+    validationOutput.textContent = "Numer nie może zawierać znaków specjalnych";
+    validationOutput.style.color = "red";
+    return;
+  }
+
+  
+  validationOutput.textContent = "Numer telefonu jest poprawny";
+  validationOutput.style.color = "green";
+}
+
+
+textField.addEventListener("input", validatePhoneNumber);
