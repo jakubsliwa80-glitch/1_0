dea76669f5a8ee103a9c8b5037af974e57d9416a
jakubsliwa80-glitch <jakubsliwa80@gmail.com>
2025-10-07 07:53:23 +0000

    Com1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-(function () {
-  //TODO
-})();
+const button = document.getElementById("ex1_button");
+const contentContainer = document.getElementById("ex1_content");
+
+function generateAndDisplayDigits() {
+  let digits = [];
+
+  for (let i = 0; i < 10; i++) {
+    digits.push(i);
+  }
+
+  const digitsToBeDisplayed = digits.join(", ");
+
+  contentContainer.innerHTML = digitsToBeDisplayed;
+}
+
+button.addEventListener("click", generateAndDisplayDigits);
